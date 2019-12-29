5c65ce1f358edf29831965cb56c9ba1a71b275d6
kgandhi09 <kushal009gandhi@gmail.com>
2019-12-29 00:13:10 -0500

    Building ANN done!

--- Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/ann.py
+++ Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/ann.py
@@ -2,10 +2,7 @@
 """
 Spyder Editor
 
-
 """
-
-
 
 #Part 1 - Data-preprocessing
 #Importing the libraries
@@ -16,14 +13,15 @@ import pandas as pd
 
 #Importing and slicing the dataset
 dataset = pd.read_csv("Churn_Modelling.csv")
-x = dataset.iloc[:, 3:14].values    #input
+x = dataset.iloc[:, 3:13].values    #input
 y = dataset.iloc[:, 13].values      #output
 
 #Encoding catagorical data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-le_x = LabelEncoder()
-x[:, 1] = le_x.fit_transform(x[:, 1])
-x[:, 2] = le_x.fit_transform(x[:, 2])
+le_x1 = LabelEncoder()
+x[:, 1] = le_x1.fit_transform(x[:, 1])
+le_x2 = LabelEncoder()
+x[:, 2] = le_x2.fit_transform(x[:, 2])
 
 ohc = OneHotEncoder(categorical_features = [1])
 x = ohc.fit_transform(x).toarray()
@@ -32,7 +30,7 @@ x = x[:, 1:]
 
 #Splitting the dataset into training set and test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, Y_Train, Y_Test = train_test_split(x, y, test_size = 0.2, random_state = 0)
+X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
 
 #Feature_Scaling
 from sklearn.preprocessing import StandardScaler
@@ -53,3 +51,39 @@ from keras.layers import Dense
 classifier = Sequential()
 
 #Adding the first input layer and first hidden layer
+# To decide the number of nodes in the hidden layer - take the average of number of nodes in input an output layer
+classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu', input_dim = 11))
+
+#Adding the second hidden layer
+classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu'))
+
+#Adding the final output layer
+classifier.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid'))
+
+#Compiling the ANN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+#Fitting the ANN to the training set
+classifier.fit(X_train, Y_train, batch_size = 10, nb_epoch = 100)
+    
+#-------------------------------------------------------------------------------------------------------
+
+#Part 3 - Making the predictions and evaluating the model
+
+#Predicting the test set results
+y_pred = classifier.predict(X_test)
+#converting predicted result from percent to boolean
+y_pred = y_pred > 0.5
+
+#Making the confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(Y_test, y_pred)
+
+
+
+
+
+
+
+
+
