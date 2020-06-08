2c91c2b4d5cdee51ba24641628a2870157214a16
kgandhi09 <kushal009gandhi@gmail.com>
2020-06-08 03:37:21 -0400

    screen capture working, working on live image processing of the game!

--- AI plays gta/screen_reading.py
+++ AI plays gta/screen_reading.py
@@ -1,7 +1,22 @@
 from PIL import ImageGrab
 from win32 import win32gui
+import cv2
+import numpy as np
+import pyautogui
 
-window = win32gui.FindWindow(None, 'File Explorer')
-win32gui.SetForeroundWindow(window)
-dimensions = win32gui.GetWindowRect(window)
+def process_image(orig_image):
+    processed_img = cv2.cvtColor(orig_image, cv2.COLOR_BGR2GRAY)
+    processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
+    return processed_img
 
+while True:
+    window = win32gui.FindWindow(None, 'Grand Theft Auto V')
+    win32gui.SetForegroundWindow(window)
+    dimensions = win32gui.GetWindowRect(window)
+
+    window_read = np.array(ImageGrab.grab(bbox=dimensions))
+    screen = process_image(window_read)
+    cv2.imshow('window', screen)
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
