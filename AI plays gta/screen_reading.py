733c913cfcd36f2e4f75b4c61ede004eb19841ae
kgandhi09 <kushal009gandhi@gmail.com>
2020-06-08 04:57:58 -0400

    mask added to image to avoid not useful items in the image

--- AI plays gta/screen_reading.py
+++ AI plays gta/screen_reading.py
@@ -5,32 +5,30 @@ import numpy as np
 import time
 from directkeys import PressKey, ReleaseKey, W, A, S, D
 
+def region_of_interest(img, vertices):
+    mask = np.zeros_like(img)
+    cv2.fillPoly(mask, vertices, 255)
+    masked = cv2.bitwise_and(img, mask)
+    return masked
 
 def process_image(orig_image):
     processed_img = cv2.cvtColor(orig_image, cv2.COLOR_BGR2GRAY)
     processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
+    vertices = np.array([[0,600], [320,330], [960,330], [1280,600], [1280,1000], [680,500], [600,500], [0,1000]])
+    processed_img = region_of_interest(processed_img, [vertices])
     return processed_img
 
 def run():
     while True:
-        window = win32gui.FindWindow(None, 'Grand Theft Auto V')
-        win32gui.SetForegroundWindow(window)
-        dimensions = win32gui.GetWindowRect(window)
-
-        window_read = np.array(ImageGrab.grab(bbox=dimensions))
+        window_read = np.array(ImageGrab.grab(bbox=(0,40,1280,1000)))
         screen = process_image(window_read)
         cv2.imshow('window', screen)
+
+
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
 
 if __name__ == '__main__':
-    for i in range(0,4):
-        print('waiting for game to start')
-        time.sleep(1)
+    run()
 
-    print('Pressing W')
-    PressKey(W)
-    time.sleep(5)
-    print('Releasing W')
-    ReleaseKey(W)
