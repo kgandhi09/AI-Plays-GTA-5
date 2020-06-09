0d411592550c889c29012278c1bd1e67c2cab709
kgandhi09 <kushal009gandhi@gmail.com>
2020-06-09 19:19:57 -0400

    dataset captured - 32,603 images captures with lanes

--- AI plays gta/dataset_capture.py
+++ AI plays gta/dataset_capture.py
@@ -2,7 +2,8 @@ import cv2
 import time
 
 #opening the video file
-cap = cv2.VideoCapture('test_video.mp4')
+
+cap = cv2.VideoCapture('lane_video.mp4')
 i=0
 init_time = time.time()
 main_time = time.time()
@@ -10,8 +11,8 @@ name_count = 1
 while(cap.isOpened()):
     curr_time = time.time()
     ret, frame = cap.read()
-    if(curr_time - init_time > 0.9):
-        cv2.imwrite('test' + str(name_count) + '.jpg', frame)
+    if(curr_time - init_time > 0.05):
+        cv2.imwrite('lane_img_' + str(name_count) + '.jpg', frame)
         init_time = curr_time
         name_count += 1
     # if curr_time - main_time > 10:
