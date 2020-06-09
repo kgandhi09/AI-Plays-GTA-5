a70e1f2a3faf6f58ce36b8d2213cf049a60d1456
kgandhi09 <kushal009gandhi@gmail.com>
2020-06-09 09:28:59 -0400

    code for capturing frames for dataset working, time to play some gta

--- AI plays gta/preparing_dataset.py
+++ AI plays gta/preparing_dataset.py
@@ -1,1 +1,22 @@
 import cv2
+import time
+
+#opening the video file
+cap = cv2.VideoCapture('test_video.mp4')
+i=0
+init_time = time.time()
+main_time = time.time()
+name_count = 1
+while(cap.isOpened()):
+    curr_time = time.time()
+    ret, frame = cap.read()
+    if(curr_time - init_time > 0.9):
+        cv2.imwrite('test' + str(name_count) + '.jpg', frame)
+        init_time = curr_time
+        name_count += 1
+    # if curr_time - main_time > 10:
+    #     break
+    i+=1
+
+cap.release()
+cv2.destroyAllWindows()
