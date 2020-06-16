bbe3cc33ed98c0b7f1ade2e97c42eb5a280eebf3
kgandhi09 <kushal009gandhi@gmail.com>
2020-06-16 05:41:58 -0400

    fixed bugs

--- AI plays gta/create_mask.py
+++ AI plays gta/create_mask.py
@@ -17,24 +17,23 @@ def create_mask(event, x, y, flags, params):
     if event == cv2.EVENT_RBUTTONDOWN:
         vertices = np.array(vertices)
         mask = np.zeros_like(img)
-        cv2.fillPoly(mask, [vertices], 255)
+        cv2.fillPoly(mask, [vertices], (255, 255, 255))
         masked = cv2.bitwise_and(img, mask)
         masking_done = True
-        
-        #cv2.imwrite("D:/Deep_Learning/AI plays gta/lane dataset/train_masked/lane_masked_1.jpg", masked)
         vertices = []
 
         
 def run():
     global img, masking_done, counter
     while True:
-        img = cv2.imread("D:/Deep_Learning/AI plays gta/lane dataset/train/lane_img_" + str(counter) + ".jpg")
+        img = cv2.imread("D:/Deep_Learning/AI plays gta/test_lane_dataset/train/lane_img_" + str(counter) + ".jpg")
         cv2.namedWindow("image", cv2.WINDOW_AUTOSIZE)
         cv2.imshow("image",img)
         cv2.setMouseCallback("image", create_mask)
         if masking_done == True:
-            cv2.imwrite("D:/Deep_Learning/AI plays gta/lane dataset/train_masked/lane_masked_" + str(counter) + ".jpg", masked)
+            cv2.imwrite("D:/Deep_Learning/AI plays gta/test_lane_dataset/masked/lane_masked_" + str(counter) + ".jpg", masked)
             counter += 1
+            masking_done = False
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
