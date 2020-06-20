7abe63fd0c85e640c0c0e6e9d6b6dd8b81b47110
kgandhi09 <kushal009gandhi@gmail.com>
2020-06-20 04:53:32 -0400

    fixed the bug and now can create two masks in a single image

--- AI plays gta/create_mask.py
+++ AI plays gta/create_mask.py
@@ -3,38 +3,63 @@ import cv2
 
 vertices_left = []
 vertices_right = []
+mask_change_flag = False
 masking_done = False
 masked = []
-counter = 83                   # counter: index of image in train folder to start with 
+counter = 268                   # counter: index of image in train folder to start with 
+
+
 
 def create_mask(event, x, y, flags, params):
  
-    global vertices_left, vertices_right, masking_done, masked, counter
-    
+    global vertices_left, vertices_right, mask_change_flag, masking_done, masked, counter
+        
     black_img_dim = img.shape
     plane_black_img = np.zeros(black_img_dim, np.uint8)
     mask = np.zeros_like(img)
     
-    if event == cv2.EVENT_LBUTTONDOWN:
+    if event == cv2.EVENT_MOUSEWHEEL:
+        mask_change_flag = True
+    
+    if event == cv2.EVENT_LBUTTONDOWN and not mask_change_flag:
         masking_done = False
         point = (x,y)
         vertices_left.append(point)
-    
+        
+    if event == cv2.EVENT_LBUTTONDOWN and mask_change_flag:
+        masking_done = False
+        point = (x,y)
+        vertices_right.append(point)
+        
     if event == cv2.EVENT_RBUTTONDOWN:
         vertices_left = np.array(vertices_left)
+        vertices_right = np.array(vertices_right)
         
-        if vertices_left.size != 0:
+        if vertices_left.size != 0 and vertices_right.size != 0:
+            cv2.fillPoly(mask, [vertices_left], (255, 255, 255))
+            cv2.fillPoly(mask, [vertices_right], (255, 255, 255))
+            masked = cv2.bitwise_and(img, mask)
+            print('mask created')
+
+        if vertices_left.size != 0 and vertices_right.size == 0:
             cv2.fillPoly(mask, [vertices_left], (255, 255, 255))
             masked = cv2.bitwise_and(img, mask)
             print('mask created')
+
+        if vertices_left.size == 0 and vertices_right.size != 0:
+            cv2.fillPoly(mask, [vertices_right], (255, 255, 255))
+            masked = cv2.bitwise_and(img, mask)
+            print('mask created')
             
-        if vertices_left.size == 0:
+        if vertices_left.size == 0 and vertices_right.size == 0:
             masked = cv2.bitwise_and(img, plane_black_img)
             print('empty mask created')
             
         masking_done = True
+        mask_change_flag = False
         vertices_left = []
-
+        vertices_right = []
+        
 def run():
     global img, masking_done, counter
     while True:
@@ -43,7 +68,7 @@ def run():
         cv2.imshow("image",img)
         cv2.setMouseCallback("image", create_mask)
         if masking_done == True:
-            cv2.imwrite("D:/Deep_Learning/AI plays gta/lane dataset/masked/lane_masked_temp_" + str(counter) + ".jpg", masked)
+            cv2.imwrite("D:/Deep_Learning/AI plays gta/lane dataset/masked/lane_masked_" + str(counter) + ".jpg", masked)
             counter += 1
             masking_done = False
         if cv2.waitKey(25) & 0xFF == ord('q'):
