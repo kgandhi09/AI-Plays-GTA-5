6a4be05c6c419e39a88ec4812559d5a3f51eff8e
kgandhi09 <kushal009gandhi@gmail.com>
2020-07-01 00:06:04 -0400

    going to overlay lane coords in the real frame of gta

--- AI plays gta/main.py
+++ AI plays gta/main.py
@@ -8,24 +8,27 @@ from skimage.transform import resize
 import matplotlib.pyplot as plt
 
 def conv_pred_to_world(world, pred, world_height, world_width):
-    cv2.imshow('pred', pred)
-    black_img = np.zeros(pred.shape, np.uint8)
-    white_pixels = np.argwhere(pred >= np.mean(pred))
-    for pixel in range(len(white_pixels)):    
-            cv2.circle(black_img, (white_pixels[pixel][1],white_pixels[pixel][0]), 1, (255, 255, 255), 1)
+    #cv2.imshow('pred', pred)
+    black_img = np.zeros(world.shape, np.uint8)
+    white_pixels_pred = np.argwhere(pred >= np.mean(pred))
+    # (x,y) of pred world are : (white_pixels_pred[pixel][1],white_pixels_pred[pixel][0])
+    for pixel in range(len(white_pixels_pred)):
+        x_world = (white_pixels_pred[pixel][1]*world_height)/128
+        y_world = (white_pixels_pred[pixel][0]*world_width)/128
+        cv2.circle(black_img, (int(x_world),int(y_world)), 1, (255, 255, 255), 1)
     cv2.imshow('world', black_img)
 
 def run(model):
     WORLD_HEIGHT = 1280
-    WORLD_WIDTH = 1000
+    WORLD_WIDTH = 1024
     IMG_HEIGHT = 128
     IMG_WIDTH = 128
     IMG_CHANNELS = 3
     X_test = np.zeros((1, IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
     while True:
         img = np.array(ImageGrab.grab(bbox=(0,40,WORLD_HEIGHT,WORLD_WIDTH)))       #Window read
-        img = cv2.resize(img, (IMG_HEIGHT, IMG_WIDTH))        
-        X_test[0] = img
+        img_resized = cv2.resize(img, (IMG_HEIGHT, IMG_WIDTH))        
+        X_test[0] = img_resized
         pred_val = model.predict(X_test)
         
         conv_pred_to_world(img, pred_val[0], WORLD_HEIGHT, WORLD_WIDTH)
