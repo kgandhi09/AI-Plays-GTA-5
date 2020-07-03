7aec9c90cd481deca3919341e99f06a0a1b560d4
kgandhi09 <kushal009gandhi@gmail.com>
2020-07-03 17:31:14 -0400

    need to create masks for new dataset with no traffic

--- AI plays gta/dataset_capture.py
+++ AI plays gta/dataset_capture.py
@@ -3,7 +3,7 @@ import time
 
 #opening the video file
 
-cap = cv2.VideoCapture('lane_video.mp4')
+cap = cv2.VideoCapture('D:\Deep_Learning\AI plays gta\lane dataset\Lane video.mp4')
 i=0
 init_time = time.time()
 main_time = time.time()
@@ -11,8 +11,8 @@ name_count = 1
 while(cap.isOpened()):
     curr_time = time.time()
     ret, frame = cap.read()
-    if(curr_time - init_time > 0.05):
-        cv2.imwrite('lane_img_' + str(name_count) + '.jpg', frame)
+    if(curr_time - init_time > 0.15):
+        cv2.imwrite('D:/Deep_Learning/AI plays gta/lane dataset/train/lane_img_' + str(name_count) + '.jpg', frame)
         init_time = curr_time
         name_count += 1
 
