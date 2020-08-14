fbfd41f64a6da10f39b9c06e164c0b61366d522f
kgandhi09 <kushal009gandhi@gmail.com>
2020-08-14 04:22:19 -0400

    preparing the training dataset for bitcoins

--- crypto prediction/prediction_model.py
+++ crypto prediction/prediction_model.py
@@ -1,2 +1,34 @@
 # -*- coding: utf-8 -*-
 
+import pandas as pd
+
+main_df = pd.DataFrame()
+ratios = ["BTC-USD", "LTC-USD", "ETH-USD", "BCH-USD"] 
+
+SEQ_LEN = 60
+FUTURE_PERIOD_PREDICT = 3
+RATIO_TO_PREDICT = "BTC-USD"
+
+def classify(current, future):
+    if float(future) > float(current):
+        return 1
+    else:
+        return 0
+    
+
+for ratio in ratios:
+    dataset = f"/home/kgandhi/Desktop/Deep_Learning/crypto prediction/crypto_data/{ratio}.csv"
+    df = pd.read_csv(dataset, names=["time", "low", "high", "open", "close", "volume"])
+    df.rename(columns={"close": f"{ratio}_close", "volume": f"{ratio}_volume"}, inplace=True)
+    df.set_index("time", inplace=True)
+    df = df[[f"{ratio}_close", f"{ratio}_volume"]]    
+    
+    if len(main_df) == 0:
+        main_df = df
+    else:
+        main_df = main_df.join(df)
+
+main_df['future'] = main_df[f"{RATIO_TO_PREDICT}_close"].shift(-FUTURE_PERIOD_PREDICT)
+main_df['target'] = list(map(classify, main_df[f"{RATIO_TO_PREDICT}_close"], main_df["future"]))
+
+print(main_df[[f"{RATIO_TO_PREDICT}_close", "future", "target"]].head(20))
